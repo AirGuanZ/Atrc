278f94f10464f1eb578a1843ecebb3ad7bc214c7
AirGuanZ <AirGuanZ@gmail.com>
2018-09-13 13:54:11 +0800

    Renderer interface

--- Src/Renderer/Renderer.h
+++ Src/Renderer/Renderer.h
@@ -2,13 +2,18 @@
 
 #include <Utils.h>
 
+#include "../Math/Ray.h"
+#include "../Misc/Spectrum.h"
+
 AGZ_NS_BEG(Atrc)
 
 class Renderer
 {
 public:
 
-    // TODO
+    virtual ~Renderer() = default;
+
+    virtual Spectrum ComputeRadiance(const Ray &ray) const = 0;
 };
 
 AGZ_NS_END(Atrc)
