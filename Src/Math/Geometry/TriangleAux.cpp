6a2579d75887d6b0aad55d61bd41f74a9384946d
AirGuanZ <AirGuanZ@gmail.com>
2018-09-22 18:32:40 +0800

    修复TriangleAux中未考虑ray参数t范围的问题

--- Src/Math/Geometry/TriangleAux.cpp
+++ Src/Math/Geometry/TriangleAux.cpp
@@ -13,28 +13,32 @@ bool HasIntersection(const Ray &r, const Vec3r &A, const Vec3r &B, const Vec3r &
 
     Vec3r o_A = r.origin - A;
     Real alpha = Mat3r::FromCols(o_A, C_A, _d).Determinant() * invDetD;
-    Real beta = Mat3r::FromCols(B_A, o_A, _d).Determinant() * invDetD;
+    Real beta  = Mat3r::FromCols(B_A, o_A, _d).Determinant() * invDetD;
+    Real t     = Mat3r::FromCols(B_A, C_A, o_A).Determinant() * invDetD;
 
     Real coefA = Real(1) - alpha - beta;
-    return coefA >= Real(0.0) && alpha >= Real(0.0) && beta >= Real(0.0);
+    return r.minT <= t && t <= r.maxT &&
+           coefA >= Real(0.0) && alpha >= Real(0.0) && beta >= Real(0.0);
 }
 
+// IMPROVE
 // Solve equations:
 //      coefA * A + coefB * B + coefC * C = o + td
 //      coefA + coefB + coefC = 1
-// and return Some(TriangleGeometryIntersection{ coefA, coefB, coefC })
-//      An equivalent equation is:
-//          A + alpha * (B-A) + beta * (C-A) = o + td
-//      =>  [B-A, C-A, -d][alpha, beta, t]' = o-A
-//      Let D = [B-A, C-A, -d]
-//      => alpha = |o-A, C-A, -d|/|D|
-//      => beta  = |B-A, o-A, -d|/|D|
-//      => t     = |B-A, C-A, o-A|/|D|
-//      And:
-//          coefA = 1 - alpha - beta
-//          coefB = alpha
-//          coefC = beta
-Option<TriangleGeometryIntersection> EvalIntersection(
+// and return Some(TriangleIntersection{ coefA, coefB, coefC })
+//
+// An equivalent equation is:
+//     A + alpha * (B-A) + beta * (C-A) = o + td
+// =>  [B-A, C-A, -d][alpha, beta, t]' = o-A
+// Let D = [B-A, C-A, -d], then:
+// => alpha = |o-A, C-A, -d| / |D|
+// => beta  = |B-A, o-A, -d| / |D|
+// => t     = |B-A, C-A, o-A| / |D|
+// Finally,
+//     coefA = 1 - alpha - beta
+//     coefB = alpha
+//     coefC = beta
+Option<TriangleIntersection> EvalIntersection(
     const Ray &r, const Vec3r &A, const Vec3r &B, const Vec3r &C)
 {
     Vec3r B_A = B - A, C_A = C - A, _d = -r.direction;
@@ -47,12 +51,14 @@ Option<TriangleGeometryIntersection> EvalIntersection(
     Vec3r o_A = r.origin - A;
     Real alpha = Mat3r::FromCols(o_A, C_A, _d).Determinant() * invDetD;
     Real beta  = Mat3r::FromCols(B_A, o_A, _d).Determinant() * invDetD;
+    Real t     = Mat3r::FromCols(B_A, C_A, o_A).Determinant() * invDetD;
 
     Real coefA = Real(1) - alpha - beta;
-    if(alpha < Real(0.0) || beta < Real(0.0) || coefA < Real(0.0))
+    if(t < r.minT || t > r.maxT ||
+       alpha < Real(0.0) || beta < Real(0.0) || coefA < Real(0.0))
         return None;
 
-    return TriangleGeometryIntersection{ coefA, alpha, beta };
+    return TriangleIntersection{ coefA, alpha, beta };
 }
 
 AGZ_NS_END(Atrc::TriangleAux)
--- Src/Math/Geometry/TriangleAux.h
+++ Src/Math/Geometry/TriangleAux.h
@@ -8,12 +8,12 @@ AGZ_NS_BEG(Atrc::TriangleAux)
 
 bool HasIntersection(const Ray &r, const Vec3r &A, const Vec3r &B, const Vec3r &C);
 
-struct TriangleGeometryIntersection
+struct TriangleIntersection
 {
     Real coefA, coefB, coefC;
 };
 
-Option<TriangleGeometryIntersection> EvalIntersection(
+Option<TriangleIntersection> EvalIntersection(
     const Ray &r, const Vec3r &A, const Vec3r &B, const Vec3r &C);
 
 AGZ_NS_END(Atrc::TriangleAux)
