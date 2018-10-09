5295481b483d7da07dfabd585fdcb8cf75760d1d
AirGuanZ <AirGuanZ@gmail.com>
2018-10-09 16:31:54 +0800

    Seperate exception-catching code in main

--- Source/Launcher/Main.cpp
+++ Source/Launcher/Main.cpp
@@ -22,71 +22,81 @@ RenderTarget<Color3b> ToSavedImage(const RenderTarget<Color3f> &origin, float ga
     });
 }
 
+void Run()
+{
+    //============= Camera =============
+
+    const Vec3r eye = { -2.0, -4.0, 1.0 };
+    const Vec3r dir = (Vec3r(0.0) - eye).Normalize();
+    PerspectiveCamera camera(
+        eye, dir, { 0.0, 0.0, 1.0 },
+        Degr(60.0), SCR_ASPECT_RATIO);
+
+    //============= Scene =============
+
+    ColoredSky sky({ 0.4f, 0.7f, 0.9f }, { 1.0f, 1.0f, 1.0f });
+
+    MatGeoEntity<Sphere> ground(
+        NewRC<DiffuseMaterial>(Spectrum(0.4f, 0.8f, 0.4f)),
+        200, Transform::Translate({ 0.0, 0.0, -200 - 1.0 }));
+
+    AGZ::Model::WavefrontObj dragonObj;
+    AGZ::Model::WavefrontObjFile::LoadFromObjFile("./Assets/dragon_vrip.obj", &dragonObj);
+    MatGeoEntity<TriangleBVH> dragon(
+        NewRC<DiffuseMaterial>(Spectrum(0.4f)),
+        dragonObj.ToGeometryMeshGroup().submeshes["Default"],
+        Transform::Translate({ 0.0, 0.0, -0.67 })
+      * Transform::Scale(Vec3r(2.2 / 50)));
+    dragonObj.Clear();
+
+    AGZ::Model::WavefrontObj bunnyObj;
+    AGZ::Model::WavefrontObjFile::LoadFromObjFile("./Assets/bunny.obj", &bunnyObj);
+    MatGeoEntity<TriangleBVH> bunny(
+        NewRC<MetalMaterial>(Spectrum(0.7f), 0.1),
+        bunnyObj.ToGeometryMeshGroup().submeshes["Default"],
+        Transform::Translate({ 2.5, 1.0, -0.85 })
+      * Transform::RotateZ(Degr(90))
+      * Transform::Scale(Vec3r(2.2 / 60)));
+    bunnyObj.Clear();
+
+    Scene scene;
+    scene.camera = &camera;
+    scene.entities = { &ground, &dragon, &bunny, &sky };
+
+    //============= Render Target =============
+
+    RenderTarget<Color3f> renderTarget(SCR_W, SCR_H);
+
+    //============= Rendering =============
+
+    ParallelRenderer<JitteredSubareaRenderer> renderer(6, 100);
+    PathTracer integrator(10);
+
+    cout << "Start rendering..." << endl;
+
+    AGZ::Timer timer;
+    renderer.Render(scene, integrator, renderTarget);
+    auto deltaTime = timer.Milliseconds() / 1000.0;
+
+    cout << "Complete rendering...Total time: " << deltaTime << "s." << endl;
+
+    //============= Output =============
+
+    AGZ::Tex::TextureFile::WriteRGBToPNG("./Build/Output.png", ToSavedImage(renderTarget, 2.2f));
+}
+
 int main()
 {
     try
     {
-        //============= Camera =============
-
-        const Vec3r eye = { -2.0, -4.0, 1.0 };
-        const Vec3r dir = (Vec3r(0.0) - eye).Normalize();
-        PerspectiveCamera camera(
-            eye, dir, { 0.0, 0.0, 1.0 },
-            Degr(60.0), SCR_ASPECT_RATIO);
-
-        //============= Scene =============
-
-        ColoredSky sky({ 0.4f, 0.7f, 0.9f }, { 1.0f, 1.0f, 1.0f });
-
-        MatGeoEntity<Sphere> ground(
-            NewRC<DiffuseMaterial>(Spectrum(0.4f, 0.8f, 0.4f)),
-            200, Transform::Translate({ 0.0, 0.0, -200 - 1.0 }));
-
-        AGZ::Model::WavefrontObj dragonObj;
-        AGZ::Model::WavefrontObjFile::LoadFromObjFile("./Assets/dragon_vrip.obj", &dragonObj);
-        MatGeoEntity<TriangleBVH> dragon(
-            NewRC<MetalMaterial>(Spectrum(0.4f), 0.2),
-            dragonObj.ToGeometryMeshGroup().submeshes["Default"],
-            Transform::Translate({ 0.0, 0.0, -0.67 })
-            * Transform::Scale(Vec3r(2.2 / 50)));
-        dragonObj.Clear();
-
-        AGZ::Model::WavefrontObj bunnyObj;
-        AGZ::Model::WavefrontObjFile::LoadFromObjFile("./Assets/bunny.obj", &bunnyObj);
-        MatGeoEntity<TriangleBVH> bunny(
-            NewRC<MetalMaterial>(Spectrum(0.7f), 0.1),
-            bunnyObj.ToGeometryMeshGroup().submeshes["Default"],
-            Transform::Translate({ 2.5, 1.0, -0.85 }) * Transform::RotateZ(Degr(90))
-            * Transform::Scale(Vec3r(2.2 / 60)));
-        bunnyObj.Clear();
-
-        Scene scene;
-        scene.camera = &camera;
-        scene.entities = { &ground, &dragon, &bunny, &sky };
-
-        //============= Render Target =============
-
-        RenderTarget<Color3f> renderTarget(SCR_W, SCR_H);
-
-        //============= Rendering =============
-
-        ParallelRenderer<JitteredSubareaRenderer> renderer(6, 100);
-        PathTracer integrator(10);
-
-        cout << "Start rendering..." << endl;
-
-        AGZ::Timer timer;
-        renderer.Render(scene, integrator, renderTarget);
-        auto deltaTime = timer.Milliseconds() / 1000.0;
-
-        cout << "Complete rendering...Total time: " << deltaTime << "s." << endl;
-
-        //============= Output =============
-
-        AGZ::Tex::TextureFile::WriteRGBToPNG("./Build/Output.png", ToSavedImage(renderTarget, 2.2f));
+        Run();
     }
     catch(const std::exception &err)
     {
         cout << err.what() << endl;
+    }
+    catch(...)
+    {
+        cout << "An unknown exception occurred. Exiting..." << endl;
     }
 }
