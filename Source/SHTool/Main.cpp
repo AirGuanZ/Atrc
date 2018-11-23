6e852ccde8a028558e97346da6ce3fbe7c28dfaa
AirGuanZ <airguanz@gmail.com>
2018-11-23 17:33:48 +0800

    移除冗余资源文件

--- Source/SHTool/Main.cpp
+++ Source/SHTool/Main.cpp
@@ -7,8 +7,8 @@ using namespace std;
 const char *USAGE_MSG =
 R"___(Usage:
     shtool project_entity [entity_desc_filename]
-    shtool project_light [light_desc_filename]
-    shtool reconstruct [entity_project_result] [light_project_result])___";
+    shtool project_light  [light_desc_filename]
+    shtool reconstruct    [entity_project_result] [light_project_result])___";
 
 int main()
 {
