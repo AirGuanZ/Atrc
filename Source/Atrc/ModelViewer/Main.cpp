2ce810ca2902947186d191d71e6894bf41c15b3a
AirGuanZ <AirGuanZ@gmail.com>
2019-01-14 01:10:31 +0800

    Update Main.cpp

--- Source/Atrc/ModelViewer/Main.cpp
+++ Source/Atrc/ModelViewer/Main.cpp
@@ -15,13 +15,14 @@ using namespace std;
 const char VS_SRC[] =
 R"___(
 #version 450 core
+uniform mat4 WVP;
 in vec2 pos;
 in vec2 iTexCoord;
 out vec2 mTexCoord;
 void main(void)
 {
     mTexCoord = iTexCoord;
-    gl_Position = vec4(pos, 0.0, 1.0);
+    gl_Position = WVP * vec4(pos, 0.0, 1.0);
 }
 )___";
 
@@ -70,13 +71,22 @@ int Run()
 
         AGZ::ObjArena<> arena;
 
-        KeyboardManager<GLFWKeyboardCapturer> input;
-        input.GetCapturer().Initialize(window);
-        input.GetKeyboard().AttachHandler(arena.Create<KeyDownHandler>(
+        KeyboardManager<GLFWKeyboardCapturer> keyboardMgr;
+        keyboardMgr.GetCapturer().Initialize(window);
+        keyboardMgr.GetKeyboard().AttachHandler(arena.Create<KeyDownHandler>(
             [&](const KeyDown &param)
         {
             if(param.key == KEY_ESCAPE)
                 glfwSetWindowShouldClose(window, GLFW_TRUE);
+        }));
+
+        MouseManager<GLFWMouseCapturer> mouseMgr;
+        mouseMgr.GetCapturer().Initialize(window);
+        mouseMgr.GetMouse().AttachHandler(arena.Create<MouseButtonDownHandler>(
+            [&](const MouseButtonDown &param)
+        {
+            if(param.button == MOUSE_LEFT)
+                cout << "Left button down!" << endl;
         }));
 
         VertexShader vs;
@@ -123,22 +133,30 @@ int Run()
         vao.BindVertexBufferToAttrib(texCoord, vtxBuf, &Vertex::texCoord, 1);
 
         UniformVariable<Texture2DUnit> tex = program.GetUniformVariable<Texture2DUnit>("tex");
+        UniformVariable<Mat4f> WVP = program.GetUniformVariable<Mat4f>("WVP");
         UniformVariableAssignment uniforms;
         uniforms.SetValue(tex, Texture2DUnit{ 0 });
+        uniforms.SetValue(WVP, Mat4f::Perspective(Deg(70), 1.0f, 0.1f, 100.0f) *
+                               Mat4f::LookAt(
+                                   { -0.5f, 0.5f, -1.0f },
+                                   { 0.0f, 0.0f, 0.0f },
+                                   { 0.0f, 1.0f, 0.0f }));
 
         Texture2D texObj;
         texObj.InitializeHandle();
-        auto texData = AGZ::Texture2D<Vec3b>(AGZ::TextureFile::LoadRGBFromFile("./Asset/tex.png", true));
-        texObj.InitializeFormatAndData(1, texData.GetWidth(), texData.GetHeight(), GL_RGB8, texData.RawData());
+        auto texData = AGZ::TextureFile::LoadRGBFromFile("./Asset/tex.png", true);
+        texObj.InitializeFormatAndData(1, GL_RGB8, texData);
         texObj.SetParameter(GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         texObj.SetParameter(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        texObj.SetParameter(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        texObj.SetParameter(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         texObj.Bind(0);
 
         glClearColor(0.0f, 1.0f, 1.0f, 0.0f);
 
         while(!glfwWindowShouldClose(window))
         {
-            glClear(GL_COLOR_BUFFER_BIT);
+            glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
             program.Bind();
             vao.Bind();
@@ -150,7 +168,8 @@ int Run()
             glfwSwapBuffers(window);
             glfwPollEvents();
 
-            input.Capture();
+            keyboardMgr.Capture();
+            mouseMgr.Capture();
         }
     }
 
