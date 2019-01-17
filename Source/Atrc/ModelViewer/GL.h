9af94df049c788564746d147f968ba3fb7d86534
AirGuanZ <AirGuanZ@gmail.com>
2019-01-18 03:13:24 +0800

    项目结构拆分：glew & imgui

--- Source/Atrc/ModelViewer/GL.h
+++ Source/Atrc/ModelViewer/GL.h
@@ -1,16 +1,18 @@
 #pragma once
 
-#define AGZ_USE_GLFW
-#define AGZ_USE_OPENGL
+#define GLEW_STATIC
 
 #include <GLLibs/glew.h>
 #include <GLLibs/glfw3.h>
 
+#define AGZ_USE_GLFW
+#define AGZ_USE_OPENGL
+
 #include <AGZUtils/Input/GLFWCapturer.h>
 #include <AGZUtils/Utils/GL.h>
 
-#include "GUI/imgui/imgui.h"
-#include "GUI/imgui_impl_glfw.h"
-#include "GUI/imgui_impl_opengl3.h"
+#include <Lib/imgui/imgui/imgui.h>
+#include <Lib/imgui/imgui_impl_glfw.h>
+#include <Lib/imgui/imgui_impl_opengl3.h>
 
 using namespace AGZ::GraphicsAPI;
