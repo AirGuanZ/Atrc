b85345fdf14bab20e3710dc6ec9264770d999af8
Z Guan <airguanz@gmail.com>
2019-03-01 19:58:35 +0800

    ResourceCore interface

--- Source/Atrc/Atrc/Atrc.h
+++ Source/Atrc/Atrc/Atrc.h
@@ -14,24 +14,16 @@ public:
     explicit Atrc(QWidget *parent = nullptr)
     {
         ui_.setupUi(this);
+
         connect(ui_.menuItem_quit, &QAction::triggered, this, &QMainWindow::close);
         connect(ui_.showDialog, &QPushButton::clicked, this, [&dialog = dialog_]
         {
-            dialog = std::make_unique<AtrcDialog>();
-            dialog->show();
+            dialog.show();
         });
-    }
-
-private slots:
-
-    void onShowDialog()
-    {
-        auto dialog = new AtrcDialog;
-        dialog->show();
     }
 
 private:
 
     Ui::MainWindow ui_;
-    std::unique_ptr<AtrcDialog> dialog_;
+    AtrcDialog dialog_;
 };
--- Source/Atrc/Atrc/AtrcDialog.h
+++ Source/Atrc/Atrc/AtrcDialog.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <QComboBox>
+#include <QListView>
+
 #include "ui_AtrcDialog.h"
 
 class AtrcDialog : public QDialog
@@ -11,10 +14,17 @@ public:
     AtrcDialog()
     {
         ui_.setupUi(this);
+
+        comboBox_.setView(new QListView);
+        comboBox_.addItem("a", 0);
+        comboBox_.addItem("b", 1);
+        ui_.layout->addWidget(&comboBox_, 0, 0);
+
         connect(ui_.closeDialog, &QPushButton::clicked, this, &QDialog::accept);
     }
 
 private:
 
     Ui::Dialog ui_;
+    QComboBox comboBox_;
 };
