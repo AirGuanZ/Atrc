f7915b52b17432364a3dfd5b0e1ae79ec6c19a13
Z Guan <airguanz@gmail.com>
2019-03-18 23:12:39 +0800

    implemented sh2d cli

--- Source/Atrc/SH2D/Main.cpp
+++ Source/Atrc/SH2D/Main.cpp
@@ -8,9 +8,9 @@
 #include <Atrc/Mgr/Parser.h>
 #include <Atrc/Mgr/SceneBuilder.h>
 #include <Atrc/SH2D/Scene2SH.h>
-#include "Light2SH.h"
+#include <Atrc/SH2D/Light2SH.h>
 
-void ProjectScene(const AGZ::Config &config, std::string_view configPath, std::string_view outputDir)
+void ProjectScene(const AGZ::Config &config, const std::string &configPath, const std::string &outputDir)
 {
     using namespace Atrc;
 
@@ -27,6 +27,18 @@ void ProjectScene(const AGZ::Config &config, std::string_view configPath, std::s
     int SHOrder = root["SHOrder"].Parse<int>();
     int workerCount = root["workerCount"].Parse<int>();
     int taskGridSize = root["taskGridSize"].Parse<int>();
+
+    if(SHOrder < 0 || SHOrder > 4)
+    {
+        std::cout << "Invalid SHOrder value: " << SHOrder << std::endl;
+        return;
+    }
+
+    if(taskGridSize <= 0)
+    {
+        std::cout << "Invalid taskGridSize value: " << taskGridSize << std::endl;
+        return;
+    }
 
     int SHC = SHOrder * SHOrder;
     std::vector<Film> coefs;
@@ -61,7 +73,7 @@ void ProjectScene(const AGZ::Config &config, std::string_view configPath, std::s
     saveToHDR("normal.hdr", normal);
 }
 
-void ProjectLight(const AGZ::Config &config, std::string_view configPath, std::string_view outputFilename)
+void ProjectLight(const AGZ::Config &config, const std::string &configPath, const std::string &outputFilename)
 {
     using namespace Atrc;
 
@@ -74,6 +86,12 @@ void ProjectLight(const AGZ::Config &config, std::string_view configPath, std::s
 
     int SHOrder = root["SHOrder"].Parse<int>();
     int SHC = SHOrder * SHOrder;
+
+    if(SHOrder < 0 || SHOrder > 4)
+    {
+        std::cout << "Invalid SHOrder value: " << SHOrder << std::endl;
+        return;
+    }
 
     std::vector<Spectrum> coefs(SHC);
     Light2SH(light, SHOrder, N, coefs.data());
@@ -97,6 +115,88 @@ void ProjectLight(const AGZ::Config &config, std::string_view configPath, std::s
         std::cout << "Failed to save light coefs to " << outputFilename << std::endl;
 }
 
+void ReconstructImage(
+    int SHOrder, const std::string &lightFilename,
+    const std::string *sceneCoefFilenames,
+    const std::string &albedoFilename,
+    const std::string &outputFilename)
+{
+    using namespace Atrc;
+
+    int SHC = SHOrder * SHOrder;
+    if(SHOrder < 0 || SHOrder > 4)
+    {
+        std::cout << "Invalid SHOrder value: " << SHOrder << std::endl;
+        return;
+    }
+
+    std::ifstream fin(lightFilename);
+    if(!fin)
+    {
+        std::cout << "Failed to open light coef file: " << lightFilename << std::endl;
+        return;
+    }
+
+    int fileSHOrder;
+    fin >> fileSHOrder;
+    if(!fin || fileSHOrder < SHOrder)
+    {
+        std::cout << "Invalid light SHOrder in file: " << lightFilename << std::endl;
+        return;
+    }
+
+    std::vector<Spectrum> lightCoefs(SHC);
+    for(int i = 0; i < SHC; ++i)
+    {
+        fin >> lightCoefs[i].r >> lightCoefs[i].g >> lightCoefs[i].b;
+        if(!fin)
+        {
+            std::cout << "Failed to load light coefs from file: " << lightFilename << std::endl;
+            return;
+        }
+    }
+
+    fin.close();
+
+    std::vector<Image> sceneCoefs(SHOrder);
+    for(int i = 0; i < SHC; ++i)
+    {
+        sceneCoefs[i] = AGZ::TextureFile::LoadRGBFromHDR(sceneCoefFilenames[i]);
+        if(i > 0 && sceneCoefs[i].GetSize() != sceneCoefs[i - 1].GetSize())
+        {
+            std::cout << "Invalid scene coef image size" << std::endl;
+            return;
+        }
+    }
+
+    Image albedo;
+    if(!albedoFilename.empty())
+    {
+        albedo = AGZ::TextureFile::LoadRGBFromHDR(albedoFilename);
+        if(albedo.GetSize() != sceneCoefs[0].GetSize())
+        {
+            std::cout << "Invalid albedo image size" << std::endl;
+            return;
+        }
+    }
+
+    Image ret(albedo.GetWidth(), albedo.GetHeight());
+    for(uint32_t y = 0; y < ret.GetHeight(); ++y)
+    {
+        for(uint32_t x = 0; x < ret.GetWidth(); ++x)
+        {
+            Spectrum pixel;
+            for(int i = 0; i < SHC; ++i)
+                pixel += sceneCoefs[i](x, y) * lightCoefs[i];
+            if(albedo.IsAvailable())
+                pixel *= albedo(x, y);
+            ret(x, y) = pixel;
+        }
+    }
+
+    AGZ::TextureFile::WriteRGBToHDR(outputFilename, ret);
+}
+
 int main(int argc, char *argv[])
 {
     try
@@ -104,9 +204,10 @@ int main(int argc, char *argv[])
         static const char USAGE_MSG[] = R"___(
     SH2D ps/project_scene scene_desc_filename output_dir_name
     SH2D pl/project_light light_desc_filename output_filename
+    SH2D rc/reconstruct   SH_order(0 to 4) light_coef_filename scene_coef_filenames... output_filename [albedo_filename]
 )___";
 
-        if(argc != 4)
+        if(argc < 2)
         {
             std::cout << USAGE_MSG << std::endl;
             return 0;
@@ -114,6 +215,12 @@ int main(int argc, char *argv[])
 
         if(argv[1] == std::string("ps") || argv[1] == std::string("project_scene"))
         {
+            if(argc != 4)
+            {
+                std::cout << USAGE_MSG << std::endl;
+                return 0;
+            }
+
             std::string filename = argv[2];
             std::string outputDir = argv[3];
 
@@ -134,6 +241,12 @@ int main(int argc, char *argv[])
         }
         else if(argv[1] == std::string("pl") || argv[1] == std::string("project_light"))
         {
+            if(argc != 4)
+            {
+                std::cout << USAGE_MSG << std::endl;
+                return 0;
+            }
+
             std::string filename = argv[2];
             std::string outputFilename = argv[3];
             auto outputDir = std::filesystem::path(outputFilename).parent_path();
@@ -152,6 +265,42 @@ int main(int argc, char *argv[])
             }
 
             ProjectLight(config, absolute(std::filesystem::path(filename)).parent_path().string(), outputFilename);
+        }
+        else if(argv[1] == std::string("rc") || argv[1] == std::string("reconstruct"))
+        {
+            if(argc < 4)
+            {
+                std::cout << USAGE_MSG << std::endl;
+                return 0;
+            }
+
+            int SHOrder = std::stoi(argv[2]);
+            if(SHOrder < 0 || SHOrder > 4)
+            {
+                std::cout << USAGE_MSG << std::endl;
+                return 0;
+            }
+
+            int SHC = SHOrder * SHOrder;
+            if(argc < 4 + SHC + 1 || argc > 4 + SHC + 2)
+            {
+                std::cout << USAGE_MSG << std::endl;
+                return 0;
+            }
+
+            std::string lightFilename = argv[3];
+
+            std::vector<std::string> sceneCoefFilenames(SHC);
+            for(int i = 0; i < SHC; ++i)
+                sceneCoefFilenames[i] = argv[4 + i];
+
+            std::string outputFilename = argv[4 + SHC];
+
+            std::string albedoFilename;
+            if(argc == 4 + SHC + 2)
+                albedoFilename = argv[4 + SHC + 1];
+
+            ReconstructImage(SHOrder, lightFilename, sceneCoefFilenames.data(), albedoFilename, outputFilename);
         }
         else
             std::cout << USAGE_MSG << std::endl;
--- Source/Atrc/SH2D/ReconstructImage.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <Atrc/Lib/Core/Common.h>
-
-Atrc::Image ReconstructImage(int SHOrder, const Atrc::Image *sceneImages, const Atrc::Spectrum *lightCoefs);
